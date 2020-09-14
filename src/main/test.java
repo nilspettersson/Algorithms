55aa29be7f34ed8484b3e96cc37525b93067b777
Nils Pettersson <nils-pettsson@outlook.com>
2020-09-14 11:02:26 +0200

    bubble sort done

--- src/main/test.java
+++ src/main/test.java
@@ -8,54 +8,34 @@ import sorting.Sorting;
 public class test {
 
 	public static void main(String[] args) {
-		Timer t = new Timer();
 		
-		//int[] array = generateArray(2000);
-		
-		int arraySize = 2000;
-		
-		
-		/*Instant startA = Instant.now();
-		for(int i = 0; i < 100; i++) {
-			int[] array = generateArray(arraySize);
-		}
-		
-		Instant finishA = Instant.now();
-		long timeElapsedA = Duration.between(startA, finishA).toMillis();
-		System.out.println(timeElapsedA);*/
-		
-		
-		
-		int steps = 100;
-		
-		Instant start = null;
-		for(int i = 0; i < steps + 2; i++) {
-			if(i == 2) {
-				start = Instant.now();
-			}
-			
-			int[] array = generateArray(arraySize);
-			Sorting.bubbleSort(array);
-			
-			
-		}
+		int[] array = generateArray(10000);
+		Instant start = Instant.now();
+		Sorting.bubbleSort(array, false);
 		
 		Instant finish = Instant.now();
-		long timeElapsed = Duration.between(start, finish).toMillis();
-		System.out.println(timeElapsed / (steps));
-		
+		long timeSmall = Duration.between(start, finish).toMillis();
+		System.out.println(timeSmall);
 		
 		//printArray(array);
-		//System.out.println(t.getTime(steps) / 1);
 	}
 	
 	public static int[] generateArray(int size) {
 		int[] array = new int[size];
-		
 		for(int i = 0; i < size; i++) {
 			array[i] = (int)(Math.random() * 100);
 		}
-		
+		return array;
+	}
+	
+	public static int[] generateArrayAsc(int size) {
+		int[] array = new int[size];
+		for(int i = 0; i < size; i++) {
+			array[i] = (int)(Math.random() * 100);
+			if(i > 0) {
+				array[i] += array[i - 1];
+			}
+		}
 		return array;
 	}
 	
--- src/sorting/Sorting.java
+++ src/sorting/Sorting.java
@@ -2,19 +2,31 @@ package sorting;
 
 public class Sorting {
 	
-	public static int[] bubbleSort(int[] array) {
-		
+	
+	public static int[] bubbleSort(int[] array, boolean asc) {
 		boolean sorted = false;
+		int remove = 0;
 		while(!sorted) {
 			sorted = true;
-			for(int i = 0; i < array.length - 1; i++) {
-				if(array[i] > array[i + 1]) {
-					int swap = array[i];
-					array[i] = array[i + 1];
-					array[i + 1] = swap;
-					sorted = false;
+			for(int i = 0; i < array.length - 1 - remove; i++) {
+				if(!asc) {
+					if(array[i] < array[i + 1]) {
+						int swap = array[i];
+						array[i] = array[i + 1];
+						array[i + 1] = swap;
+						sorted = false;
+					}
+				}
+				else {
+					if(array[i] > array[i + 1]) {
+						int swap = array[i];
+						array[i] = array[i + 1];
+						array[i + 1] = swap;
+						sorted = false;
+					}
 				}
 			}
+			remove++;
 		}
 		
 		return array;
